a46809ed2a32314f421a34c316146d1441627c17
gsujanth <princesujanth@gmail.com>
2018-03-18 18:57:29 -0400

    third commit

--- app/src/main/java/com/example/princ/midpractice/MovieAdapter.java
+++ app/src/main/java/com/example/princ/midpractice/MovieAdapter.java
@@ -64,8 +64,6 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
             public void onClick(View view) {
                 Log.d("demo", "onClick: "+position);
                 dUpdate.updateFavorite(position);
-                //hiji
-
             }
         });
 
