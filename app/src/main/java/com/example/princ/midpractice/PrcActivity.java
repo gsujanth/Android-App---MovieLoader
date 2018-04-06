695caf5ed2e906888f44d63a9e0efdd00079607d
gsujanth <princesujanth@gmail.com>
2018-04-06 05:09:25 -0400

    third commit

--- app/src/main/java/com/example/princ/midpractice/PrcActivity.java
+++ app/src/main/java/com/example/princ/midpractice/PrcActivity.java
@@ -1,13 +1,42 @@
 package com.example.princ.midpractice;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class PrcActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    RelativeLayout relativeLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prc);
+
+        relativeLayout=findViewById(R.id.prc);
+        fab=findViewById(R.id.fab);
+
+        Snackbar.make(relativeLayout, "Welcome", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(PrcActivity.this, "I am fab", Toast.LENGTH_SHORT).show();
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Click", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        }).show();
+            }
+        });
     }
 }
